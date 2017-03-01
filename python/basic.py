710615064b12e28e13d2c8054ffc9bec03ba1981
user01 <user01@teaching-980E.(none)>
2017-03-01 11:09:57 +0000

    Added new scripts.

--- python/basic.py
+++ python/basic.py
@@ -30,6 +30,12 @@ print str (a) + " squared equals " + str (b) + " squared " + str (c) + "squared.
 
 #question 5
 
+num = 24
+a = 34.999
+
+result = num* (13 - a**2) + 1.0
+print "Result:", resultmv 
+
 
 
 
---
+++ python/controlflow.py
@@ -0,0 +1,27 @@
+#question 1
+
+num_moons = 10
+while num_moons > 5:
+    print num_moons
+
+
+#question 2
+
+my_list = [23, "hi", 2.4e-10]
+count = 0
+
+while count <3:
+    item=my_list[count]
+    print item, count
+    count +=1
+
+#question 3
+
+x = 1
+if x: print x, "is true"
+
+
+
+
+
+
